6109ef09fe84103a777d36225b737b24d478048c
Thomas Kretschmann <t.kretschmann@t-online.de>
2023-08-02 13:23:13 +0200

    Fußpflegebuch

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,15 +1,20 @@
 import { writable } from 'svelte/store';
 
 type Product = {
-    title: string;
-    price: number;
-    content: string;
-    img: string;
-}
+	title: string;
+	price: number;
+	content: string;
+	img: string;
+};
 
-// export const divider = writable(4);
 export const products = writable([
-	{ title: 'Analadventures', price: 69, content: '<p>Test</p>', img: 'innsbruck-für-depperl.png' },
+	{
+		title: 'Handbuch für medizinische Fußpflege',
+		price: 79.99,
+		content:
+			'<p>Podologiewissen kompakt<p><p>Leicht verständlich und gut illustriert vermittelt der Titel die Basics der podologischen Behandlung: Praxismanagement, Patientenkommunikation sowie ergänzende spezielle Techniken und Spezialverbände.</p><br><p>Kompakt und übersichtlich:</p><ul><li>fundiertes Wissen um Fuß und Pflege</li><li>eindrucksvolles Bildmaterial, insbesondere zu krankhaften Veränderungen</li><li>Abdeckung aller Inhalte der Ausbildungsordnung Podologie</li></ul><p>Dieses Handbuch ist "aus der Praxis für die Praxis" entstanden. Es richtet sich nicht nur an Podologen sondern auch an alle, die im Bereich der medizinischen Fußpflege bzw. Fußbehandlung tätig sind. Das versierte Autorenteam setzt sich überwiegend aus Dozenten der Schule für Podologie in Neuenbürg zusammen.</p>',
+		img: 'fußpflege.png'
+	},
 	{
 		title: 'Innsbruck für Depperl',
 		price: 24.95,
