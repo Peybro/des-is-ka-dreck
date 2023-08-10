f416dc23479738d7a0aceed61aa1a53907fa0c20
Peybro <31886496+Peybro@users.noreply.github.com>
2023-08-10 20:48:11 +0200

    footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,14 @@
+<footer>
+    <h1>Alle Angaben .......</h1>
+</footer>
+
+<style>
+    footer {
+        width: 100vw;
+        height: 200px;
+        background-color: black;
+        display: flex;
+        justiy-content: center;
+        align-items: center;
+    }
+</style>
